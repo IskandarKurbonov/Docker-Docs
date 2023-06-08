8da45b0c9f5e35d5f6915e0fcc4b5d4c845161c3
Iskandar Kurbonov <iskandar.kurbonov@onlyoffice.com>
2023-06-08 16:25:09 +0300

    Add testing files

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
+# Run test environment
+for config in ${config[@]}; do
+  # Check if the yml exists
+  if [[ ! -f ./tests/${config} ]]; then
+    echo "File ${config} doesn't exist!"
+    exit 0
+  fi
+  docker compose -p ds -f ./tests/${config} up -d
+  sleep 10
+  docker ps -a
+done
 
-healthcheck_res=$(curl localhost/healthcheck)
+url=${url:-"http://localhost"}
 
+wakeup_timeout=120
+
+# Get Docs healthcheck status
+echo "Wait for service wake up"
+
+sleep $wakeup_timeout
+
+healthcheck_res=$(wget --no-check-certificate -qO - ${url}/healthcheck)
+
+# Fail if it isn't true
 if [[ $healthcheck_res == "true" ]]; then
-    echo "Healthcheck passed."
+  echo "Healthcheck passed."
 else
-    echo "Healthcheck failed!"
-    exit 1
+  echo "Healthcheck failed!"
+  exit 1
 fi
+
